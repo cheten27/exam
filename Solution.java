7b4bbc4311592f3f3251ee729cb0284adf07e094
cheten27 <165474582+cheten27@users.noreply.github.com>
2024-07-20 13:53:17 +0530

    Add files via upload
    first commit

---
+++ Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Solution{
+   public static void main(String args[]){
+       int numerator= 10;
+       int denominator=20;
+
+       if(numerator>denominator){
+              System.out.println("numerator is greater than denominator");
+       }
+       else if(numerator==denominator){
+              System.out.println("numerator is equal to denominator");
+       }
+       else{
+              System.out.println("numerator is less than denominator");
+       }
+   }
+}
